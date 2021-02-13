843f4904ee3ef17e837bd29b9eb47d4a3fa937e4
Quiana <quianalee@Quianas-MacBook-Air.local>
2021-02-12 19:29:33 -0500

    final changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ class Projects extends Component {
                 <h1 className='projects'>Projects</h1>
                 <div className='flex'>
                     <img className='pic' src='img/spacedrep.png' alt='spacedrep'></img>
-                    <p>This is an application to help people learn how to speak a new language. The skills I used are Node, Express, PostgreSQL, and React.</p>
+                    <p>This is Spaced Repetition. An application to help people learn how to speak a new language. I chose ancient Hebrew because it is very unique. The skills I used are Node, Express, PostgreSQL, and React.</p>
                     <ul>
                         <li><a href='https://client-spaced-repetition.vercel.app/'>Spaced Rep</a></li>
                         <li><a href='https://github.com/patience144/client-spaced-repetition'>Github Client</a></li>
@@ -20,7 +20,7 @@ class Projects extends Component {
 
                 <div className='flex'>
                     <img className='pic' src='img/petful.png' alt='spacedrep'></img>
-                    <p>This is an application to simulate the FIFO algorithm. The skills I used are also Node, Express, PostgreSQL, and React.</p>
+                    <p>This is F.I.F.O. Find A Pet. An application to simulate the FIFO algorithm. I built this to show the first in, first out process. The skills I used are also Node, Express, PostgreSQL, and React.</p>
                     <ul>
                         <li><a href='https://petful-client-phi.vercel.app/'>Petful</a></li>
                         <li><a href='https://github.com/patience144/Petful-clientQ'>Github Client</a></li>
@@ -34,7 +34,7 @@ class Projects extends Component {
 
                 <div className='flex'>
                     <img className='pic' src='img/choreapp.png' alt='spacedrep'></img>
-                    <p>This is an application to help people keep track of their daily chores. The skills I used are Node, Express, PostgreSQL, and React.</p>
+                    <p>This is The Chore App. An application to help people keep track of their daily chores. I have four kids and it comes in handy. The skills I used are Node, Express, PostgreSQL, and React.</p>
                     <ul>
                         <li><a href='https://pc-client-three.vercel.app/'>Chore App</a></li>
                         <li><a href='https://github.com/patience144/choreapp-clientside'>Github Client</a></li>
