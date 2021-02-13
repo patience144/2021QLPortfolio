c607614bf39b820b8cce687448af5962c5084a4a
Quiana <quianalee@Quianas-MacBook-Air.local>
2021-02-13 11:09:02 -0500

    changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react'
-import avatar from '../avatar.png'
-
-
+import React, { Component } from "react";
+import avatar from "../avatar.png";
 
 class LandingPage extends Component {
-    render() {
-        return (
-            <div className="landing" id='home'>
-                <h1 className="ty">Thank You For Coming</h1>
-                <p className='quote'>"Be yourself; everyone else is already taken."  -Oscar Wilde</p>
+  render() {
+    return (
+      <div className='landing' id='home'>
+        <h1 className='ty'>Thank You For Coming</h1>
+        <p className='quote'>
+          "Be yourself; everyone else is already taken." -Oscar Wilde
+        </p>
 
-                <img className='avatar' src={avatar} alt='avatar'></img>
+        <img className='avatar' src={avatar} alt='avatar'></img>
 
-                <p className='about'> I am a Web Developer who is addicted to learning how to think! I have a fondness for Front-End Web Development, and I also have Back-End knowledge that I would like to continue to develope. My goal is to become apart of an amazing web development team and change the future!<br />
-                 When I was a stay at home mother, I wrote a self help book, which I am very proud of. In order to sell it, I needed an E-commerse website, but at the time I couldn't afford having one built for me, or even WIX. A family friend told me I could build my own website, and it was that moment that changed my life and lead me here today!<br /> Discovering this world has been exciting and stressful, but one thing is for sure, this is where I am supposed to be. I love coding, and I am, a coder!</p>
-            </div>
-        )
-    }
+        <p className='about'>
+          I am a Web Developer who is addicted to learning how to think! I have
+          a fondness for Front-End Web Development, and I also have Back-End
+          knowledge that I would like to continue to develop. My goal is to
+          become apart of an amazing web development team and change the future!
+          <br />
+          When I was a stay at home mother, I wrote a self-help book, which I am
+          very proud of. To sell it, I needed an E-commerce website, but at the
+          time I couldn't afford to have one built for me or even WIX. A family
+          friend told me I could build my website myself, and it was that moment
+          that changed my life and leads me here today!
+          <br /> Discovering this world has been exciting and stressful, but one thing is for sure, this is where I am supposed to be. I love coding, and I am a coder!  I look forward to working in teams, and I am thrilled to see what amazing things we can create! I am a cyber architect and coding is my life!
+        </p>
+      </div>
+    );
+  }
 }
 
-export default LandingPage
+export default LandingPage;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,8 @@ class Projects extends Component {
                 <h1 className='projects'>Projects</h1>
                 <div className='flex'>
                     <img className='pic' src='img/spacedrep.png' alt='spacedrep'></img>
-                    <p>This is Spaced Repetition. An application to help people learn how to speak a new language. I chose ancient Hebrew because it is very unique. The skills I used are Node, Express, PostgreSQL, and React.</p>
+                    <h4>Title: Spaced Repetition</h4>
+                    <p>This is an application for people who want to learn how to speak a new language. I built this because people love to learn new languages. The skills I used are Node, Express, PostgreSQL, and React.</p>
                     <ul>
                         <li><a href='https://client-spaced-repetition.vercel.app/'>Spaced Rep</a></li>
                         <li><a href='https://github.com/patience144/client-spaced-repetition'>Github Client</a></li>
@@ -20,7 +21,8 @@ class Projects extends Component {
 
                 <div className='flex'>
                     <img className='pic' src='img/petful.png' alt='spacedrep'></img>
-                    <p>This is F.I.F.O. Find A Pet. An application to simulate the FIFO algorithm. I built this to show the first in, first out process. The skills I used are also Node, Express, PostgreSQL, and React.</p>
+                    <h4>Title: F.I.F.O. Find A Pet</h4>
+                    <p>This is an application for people who want to adopt a pet. It simulates the FIFO algorithm. I built this to show the first in, first out process. The skills I used are also Node, Express, PostgreSQL, and React.</p>
                     <ul>
                         <li><a href='https://petful-client-phi.vercel.app/'>Petful</a></li>
                         <li><a href='https://github.com/patience144/Petful-clientQ'>Github Client</a></li>
@@ -34,7 +36,8 @@ class Projects extends Component {
 
                 <div className='flex'>
                     <img className='pic' src='img/choreapp.png' alt='spacedrep'></img>
-                    <p>This is The Chore App. An application to help people keep track of their daily chores. I have four kids and it comes in handy. The skills I used are Node, Express, PostgreSQL, and React.</p>
+                    <h4>Title: Chore App</h4>
+                    <p>This is an application for people keep track of their daily chores. I built this because I have four kids and it comes in handy. The skills I used are Node, Express, PostgreSQL, and React.</p>
                     <ul>
                         <li><a href='https://pc-client-three.vercel.app/'>Chore App</a></li>
                         <li><a href='https://github.com/patience144/choreapp-clientside'>Github Client</a></li>
